51d647583755b4a07bdbc16053f28058492b1870
jujudev7 <penna.julien@gmail.com>
2024-11-13 16:41:47 +0100

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="p-4 flex flex-col gap-4">
+      <input
+        checked={show}
+        onChange={(e) => {
+          setShow(e.target.checked);
+        }}
+        type="checkbox"
+      ></input>
+      {show ? <MousePosition /> : null}
       <Lego color="blue" />
       <Lego
         color="red"
@@ -11,11 +22,11 @@ export default function App() {
           console.log("Patate clicked", e);
         }}
       >
-        Patate
+        <p className="ml-2">Patate</p>
       </Lego>
       <Lego size="lg">
         <button
-          className="border bg-zinc-900 text-white rounded-md px-2 py-1"
+          className="border bg-zinc-900 text-white rounded-md ml-2 px-2 py-1"
           onClick={(e) => {
             console.log("Click !", e);
           }}
@@ -29,15 +40,47 @@ export default function App() {
   );
 }
 
+function MousePosition() {
+  const [mousePosition, setMouvePosition] = useState({ x: 1, y: 1 });
+
+  useEffect(() => {
+    console.log("Setup");
+
+    console.log("Create event listener");
+    document.body.addEventListener("mousemove", (e) => {
+      console.log("Run mousemove with");
+
+      setMouvePosition({ x: e.clientX, y: e.clientY });
+    });
+
+    return () => {
+      // Cleanup
+      console.log("Cleanup");
+    };
+  }, []);
+
+  return (
+    <div className="">
+      X : {mousePosition.x} / Y : {mousePosition.y}
+    </div>
+  );
+}
+
+// Render
+// view = function(state)
 function Lego({ color = "red", size = "sm", children, onClick }) {
-  // console.log(props);
+  const [value, setValue] = useState(95);
+
+  console.log("Render", value);
   return (
     <div
       className={`h-16 flex items-center justify-center ${COLORS[color]} ${SIZE[size]}`}
       onClick={() => {
         onClick?.("mon super paramètre");
+        setValue(value + 1);
       }}
     >
+      {value}
       {children}
     </div>
   );
@@ -60,3 +103,4 @@ Lego.propTypes = {
   children: PropTypes.node, // `children` peut être n'importe quel contenu React
   onClick: PropTypes.func, // `onClick` doit être une fonction
 };
+
